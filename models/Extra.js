b2938be679ef2e6efcd56d15c8d1a79e93cb21b1
Ashu3208 <92543518+Ashu3208@users.noreply.github.com>
2023-04-12 00:38:06 +0530

    Add files via upload

---
+++ models/Extra.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const extraSchema = new mongoose.Schema({
+    
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true,
+        min:1
+    }
+})
+
+module.exports = mongoose.model('Extra',extraSchema)
+
